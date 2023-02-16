b0b06371846573f250ad1d5d1c3ede81ec8938e4
Yeongrok Jeong <yeongrok.jeong@gmail.com>
2023-02-17 08:51:08 +0900

    1. Two Sum / Easy / JavaScript (#1)

---
+++ yeongrok/0001-two-sum/0001-two-sum.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    let map = new Map();
+    for (let i = 0; i < nums.length; i++) {
+        if (map.has(nums[i])) {
+            return [map.get(nums[i]), i];
+        } else {
+            map.set(target - nums[i], i);
+        }
+    }
+};
