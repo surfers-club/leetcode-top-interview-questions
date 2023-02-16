3c6b1007f6a897b86d0237f2d1333357b99da5a7
Yeongrok Jeong <yeongrok.jeong@gmail.com>
2023-02-16 14:25:10 +0900

    Time: 63 ms (94.64%), Space: 43.3 MB (22.59%) - LeetHub

--- yeongrok/0001-two-sum/0001-two-sum.js
+++ yeongrok/0001-two-sum/0001-two-sum.js
@@ -4,31 +4,12 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    //  ↓  O(n^2) 인 방법
-    // for (let i = 0; i < nums.length; i++) {
-    //     for (let j = i + 1; j < nums.length; j++) {
-    //         if (nums[i] + nums[j] === target) return [i, j];
-    //     }
-    // }
-
-    const originNums = [...nums];
-    nums.sort((a, b) => a - b);
-
-    let i = 0;
-    let j = i + 1;
-    
-    while (nums[i] + nums[j] !== target) {
-        if (nums[i] + nums[j] < target) {
-            i += 1;
-            j += 1;
-        } else if (nums[i] + nums[j] > target) {
-            i -= 1;
+    let map = new Map();
+    for (let i = 0; i < nums.length; i++) {
+        if (map.has(nums[i])) {
+            return [map.get(nums[i]), i];
+        } else {
+            map.set(target - nums[i], i);
         }
     }
-    return originNums.reduce((acc, curr, index) => {
-        if ([nums[i], nums[j]].includes(curr)) {
-            acc.push(index);
-        }
-        return acc;
-    }, []);
-}
+};
