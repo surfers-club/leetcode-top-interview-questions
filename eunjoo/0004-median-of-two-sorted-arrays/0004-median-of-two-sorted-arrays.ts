763c79dd5675de87938758cf61fc6cbb3c7b1629
eunjoo jung <52408635+lynn0506@users.noreply.github.com>
2023-02-27 23:02:37 +0900

    Time: 125 ms (34.96%), Space: 46.3 MB (94.65%) - LeetHub

---
+++ eunjoo/0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.ts
@@ -0,0 +1,24 @@
+function findMedianSortedArrays(nums1: number[], nums2: number[]): number {
+    const sortedNums = Array(nums1.length + nums2.length);
+
+    let index1 = 0;
+    let index2 = 0;
+
+    while(index1 < nums1.length || index2 < nums2.length) {
+        if(index2 === nums2.length || nums1[index1] <= nums2[index2]) {
+            sortedNums[index1 + index2] = nums1[index1];
+            index1++;
+        } else {
+            sortedNums[index1 + index2] = nums2[index2];
+            index2++;
+        }
+    }
+
+    const mediumNum = Math.floor(sortedNums.length / 2);
+
+    if(sortedNums.length % 2 == 1) {
+        return sortedNums[mediumNum];
+    } else {
+        return (sortedNums[mediumNum-1] + sortedNums[mediumNum]) / 2.0;
+    }
+};
