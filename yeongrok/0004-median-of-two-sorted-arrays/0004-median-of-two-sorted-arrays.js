d701c07c0d784467c19b148d8571f4bfbd4ca4e7
Yeongrok Jeong <yeongrok.jeong@gmail.com>
2023-02-27 23:18:59 +0900

    Time: 108 ms (66.81%), Space: 46.3 MB (79.27%) - LeetHub

---
+++ yeongrok/0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.js
@@ -0,0 +1,35 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number}
+ */
+var findMedianSortedArrays = function(nums1, nums2) {
+    const halfLength = (nums1.length + nums2.length) / 2;
+    let m;
+    let n;
+    let mergedArray = [];
+    
+    while (mergedArray.length <= halfLength) {
+        m = nums1.length - 1;
+        n = nums2.length - 1;
+        if (m >= 0 && n >= 0) {
+             if (nums1[nums1.length - 1] >= nums2[nums2.length - 1]) {
+                mergedArray.push(nums1.pop());
+            } else {
+                mergedArray.push(nums2.pop());
+            }   
+        } else if (m >= 0) {
+            mergedArray.push(nums1.pop());
+        } else if (n >= 0) {
+            mergedArray.push(nums2.pop());
+        } else {
+            break;
+        }
+    }
+
+    if (Number.isInteger(halfLength)) {
+        return (mergedArray[mergedArray.length - 1] + mergedArray[mergedArray.length - 2]) / 2;
+    } else {
+        return mergedArray[mergedArray.length - 1];
+    }
+};
