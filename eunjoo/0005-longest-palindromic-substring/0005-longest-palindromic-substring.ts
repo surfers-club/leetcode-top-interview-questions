97976601cba9609d193bea70ab7bbbb1e3e95f96
eunjoo jung <52408635+lynn0506@users.noreply.github.com>
2023-03-02 23:11:40 +0900

    Time: 81 ms (96.92%), Space: 45.2 MB (87.50%) - LeetHub

---
+++ eunjoo/0005-longest-palindromic-substring/0005-longest-palindromic-substring.ts
@@ -0,0 +1,27 @@
+const getThePalindrome = (s: string, left: number, right: number): string => {
+    while(left >= 0 && right < s.length && s[left] === s[right]) {
+        left -= 1;
+        right += 1;
+    }
+
+    return s.slice(left+1, right);
+}
+
+
+function longestPalindrome(s: string): string {
+    let palindrome = '';
+
+    for(let left = 0; left <= s.length; left++) {
+        const oddPalindrome = getThePalindrome(s, left, left);
+        if(oddPalindrome.length > palindrome.length) {
+            palindrome = oddPalindrome;
+        }
+
+        const evenPalindrome = getThePalindrome(s, left, left+1);
+        if(evenPalindrome.length > palindrome.length) {
+            palindrome = evenPalindrome;
+        }
+    }
+
+    return palindrome;
+};
