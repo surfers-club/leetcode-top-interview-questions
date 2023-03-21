f6a4e8c4eb93b7fa67a190a1bfda46f844839637
eunjoo jung <52408635+lynn0506@users.noreply.github.com>
2023-03-21 10:26:04 +0900

    Time: 135 ms (62.01%), Space: 47.3 MB (89.90%) - LeetHub

--- eunjoo/0013-roman-to-integer/0013-roman-to-integer.ts
+++ eunjoo/0013-roman-to-integer/0013-roman-to-integer.ts
@@ -1,14 +1,14 @@
-function romanToInt(s: string): number {
-    const romanMap = { 
-        'I': 1,
-        'V': 5,
-        'X': 10,
-        'L': 50,
-        'C': 100,
-        'D': 500,
-        'M': 1000
-    }
+const romanMap = { 
+    'I': 1,
+    'V': 5,
+    'X': 10,
+    'L': 50,
+    'C': 100,
+    'D': 500,
+    'M': 1000
+}
 
+function romanToInt(s: string): number {
     let sum = 0;
 
     for(let i = 0; i < s.length; i++) {
