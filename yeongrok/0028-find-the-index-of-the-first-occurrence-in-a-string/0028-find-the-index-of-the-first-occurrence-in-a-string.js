a73f12baf8d7f37d090893f39296156e20b26182
yeongrok-jeong <yeongrok.jeong@gmail.com>
2023-04-24 20:06:41 +0900

    Time: 57 ms (67.41%), Space: 42.3 MB (32.40%) - LeetHub

--- yeongrok/0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.js
+++ yeongrok/0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.js
@@ -4,5 +4,27 @@
  * @return {number}
  */
 var strStr = function(haystack, needle) {
-    return haystack.indexOf(needle);
+    if (haystack.length < needle.length) return -1;
+
+    let curr = 0;
+    let char = needle[curr];
+    let output = -1;
+
+    for (let i = 0; i < haystack.length; i++) {
+        if (haystack[i] === char) {
+            if (curr === 0) output = i;
+            curr++;
+        } else {
+            if (curr !== 0) i = output;
+            output = -1;
+            curr = 0;
+        }
+
+        if (curr === needle.length) {
+            return output;
+        }
+        char = needle[curr];
+    }
+
+    return -1;
 };
